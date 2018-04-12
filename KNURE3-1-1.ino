d912841995f4dd07343144597a0299210f7abbbe
Oleksander28 <37450310+Oleksander28@users.noreply.github.com>
2018-04-13 01:19:04 +0300

    Add files via upload

---
+++ KNURE3-1-1.ino
@@ -0,0 +1,33 @@
+#include "DHT.h"
+#define DHTPIN 6 // пин подключения контакта DATA
+#define DHTTYPE DHT22 // DHT 22
+#include <LiquidCrystal.h>
+// инициализация с указанием контактов подключения
+LiquidCrystal lcd(  12, 11, 5, 4, 3, 2);
+DHT dht(DHTPIN, DHTTYPE);
+void setup()
+{
+lcd.begin(20,2); // режим работы
+dht.begin();
+}
+void loop()
+{
+// получение с датчика  температуры
+float h = dht.readHumidity();
+float t = dht.readTemperature();
+if (isnan(t) || isnan(h)) // ошибка получения данных
+{
+lcd.clear();lcd.setCursor(0,0); 
+lcd.print("Failed to read");
+}
+else // вывести данные на ЖКИ
+{
+lcd.clear();
+
+lcd.setCursor(0,0);
+lcd.print("TEMP:"); lcd.print(t); lcd.println("C     ");
+
+}
+
+delay(2000); // пауза перед следующим измерением
+}
---
+++ KNURE3-2-1.ino
@@ -0,0 +1,39 @@
+#include "DHT.h"
+#define DHTPIN 6 // пин подключения контакта DATA
+#define DHTTYPE DHT22 // DHT 22
+#include <LiquidCrystal.h>
+// инициализация с указанием контактов подключения
+LiquidCrystal lcd(  12, 11, 5, 4, 3, 2);
+DHT dht(DHTPIN, DHTTYPE);
+void setup()
+{
+lcd.begin(20,2); // режим работы
+dht.begin();
+}
+void loop()
+{
+// получение с датчика данных влажности и температуры
+float h = dht.readHumidity();
+float t = dht.readTemperature();
+if (isnan(t) || isnan(h)) // ошибка получения данных
+{
+lcd.clear();lcd.setCursor(0,0);
+lcd.print("Failed to read");
+}
+else // вывести данные на ЖКИ
+{
+lcd.clear();
+
+lcd.setCursor(0,0);
+lcd.print("TO:"); lcd.print(t);
+
+lcd.setCursor(8,0);
+lcd.print("TI:"); lcd.print(t);
+
+
+lcd.setCursor(3,1);
+lcd.print("HUM:");lcd.print(h); lcd.println("%   ");
+}
+
+delay(2000); // пауза перед следующим измерением
+}
